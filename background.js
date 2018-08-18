752cb5fc0cd1b0bb27738c983fc85e2127eb0701
Will S <wsha.code@gmail.com>
2018-08-18 02:38:55 -0400

    Notifications for new feed items

--- background.js
+++ background.js
@@ -1,3 +1,30 @@
+var defaults = {
+    'interval': 5, 
+    'lastEntry': 0,
+    'maxNotifications': 5,
+    'notifications': true,
+    'password': '',
+    'url': '',
+    'username': '',
+    'useIcons': true
+}
+
+async function setDefaults() {
+    var settingsNames = Object.getOwnPropertyNames(defaults)
+    var settings = await browser.storage.local.get(settingsNames)
+
+    var val
+    for (let setting in defaults) {
+        if (settings[setting] === undefined) {
+            val = defaults[setting]
+        } else {
+            val = settings[setting]
+        }
+        settings[setting] = val
+    }
+    browser.storage.local.set(settings)
+}
+
 function sanitizeInterval(settings) {
     var interval
     if (settings.hasOwnProperty('interval')) {
@@ -9,19 +36,93 @@ function sanitizeInterval(settings) {
 }
 
 async function checkFeeds() {
-    var info = await browser.storage.local.get(['url', 'username', 'password'])
-    var request = new XMLHttpRequest()
+    var info = await browser.storage.local.get([
+        'url', 'username', 'password', 'lastEntry', 'notifications',
+        'maxNotifications', 'useIcons'])
 
-    function reqListener () {
-        var response = JSON.parse(this.responseText)
-        browser.browserAction.setBadgeText({'text': `${response.total}`})
+    var limit
+    if (info.notifications) {
+        limit = info.maxNotifications
+    } else {
+        limit = 1
     }
-    request.addEventListener('load', reqListener)
+    var url = info.url + '/v1/entries?status=unread&direction=desc' +
+        `&limit=${limit}&after_entry_id=${info.lastEntry}`
+    var headers = new Headers()
+    headers.append('Authorization', 'Basic ' + btoa(`${info.username}:${info.password}`))
+    var response = await fetch(url, {credentials: "include", headers: headers})
+    var body = await response.json()
 
-    request.open('GET', info.url + '/v1/entries?status=unread&limit=1')
-    request.withCredentials = true
-    request.setRequestHeader('Authorization', 'Basic ' + btoa(info.username + ':' + info.password))
-    request.send()
+    browser.browserAction.setBadgeText({'text': `${body.total}`})
+
+    var numEntries = body.entries.length
+    if (numEntries > 0) {
+        var lastEntry = Math.max(body.entries[0].id, info.lastEntry)
+        browser.storage.local.set({'lastEntry': lastEntry})
+    }
+
+    if (info.notifications && numEntries > 0) {
+        var numShow
+        if (numEntries > info.maxNotifications) {
+            numShow = info.maxNotifications - 1
+        } else {
+            numShow = numEntries
+        }
+
+        var iconIds = []
+        var iconData = []
+        if (info.useIcons) {
+            for (let idx=numShow - 1; idx >= 0; idx--) {
+                let entry = body.entries[idx]
+                if (iconIds.includes(entry.feed_id)) {
+                    continue
+                }
+
+                if (entry.feed.icon) {
+                    iconIds.push(entry.feed_id)
+                    iconData.push(fetch(
+                        info.url + `/v1/feeds/${entry.feed_id}/icon`,
+                        {credentials: 'include', headers: headers}).
+                        then((response) => response.json()))
+                }
+            }
+        }
+        if (iconIds) {
+            iconData = await Promise.all(iconData)
+        }
+        var icons = {}
+        for (let idx=0; idx<iconIds.length; idx++) {
+            icons[iconIds[idx]] = iconData[idx].data
+        }
+        // icondIds.forEach((key, idx) => icons[key] = iconData[idx])
+
+        for (let idx=numShow - 1; idx >= 0; idx--) {
+            let entry = body.entries[idx]
+            let iconUrl
+            if (icons.hasOwnProperty(entry.feed_id)) {
+                iconUrl = 'data:' + icons[entry.feed_id]
+            } else {
+                iconUrl = 'icons/icon64.png'
+            }
+            browser.notifications.create('', {
+                "type": "basic",
+                "title": entry.feed.title,
+                "message": entry.title,
+                "iconUrl": iconUrl
+            })
+        }
+
+        if (body.total > info.maxNotifications) {
+            var msg = `${body.total - info.maxNotifications}` +
+                ' additional new feed items....'
+            browser.notifications.create('', {
+                "type": "basic",
+                "title": 'Miniflux',
+                "message": msg,
+                "iconUrl": 'icons/icon64.png'
+            })
+        }
+    }
 }
 
 async function calculateDelay(interval) {
@@ -48,7 +149,7 @@ async function setupAlarm() {
     var loginInfo = ['url', 'username', 'password']
     var settings = await browser.storage.local.get(['interval', ...loginInfo])
 
-    // Need all the login settings to run alarm
+    // Need non-empty values for all the login settings to run alarm
     if (loginInfo.some(el => !settings[el])) {
         return
     }
@@ -61,9 +162,8 @@ async function setupAlarm() {
                            'periodInMinutes': interval})
 }
 
+setDefaults()
 browser.browserAction.setBadgeBackgroundColor({'color': 'blue'})
 browser.browserAction.onClicked.addListener(checkFeeds)
 setupAlarm()
 browser.alarms.onAlarm.addListener(handleAlarm)
-
-// Notifications
--- options.js
+++ options.js
@@ -1,10 +1,17 @@
-var settingsNames = ['url', 'username', 'password', 'interval', 'notifications']
+var optionNames = ['interval', 'lastEntry', 'maxNotifications',
+    'notifications', 'password', 'url', 'username', 'useIcons']
 
 function saveOptions(e) {
     e.preventDefault()
     var settings = {}
-    for (let setting of settingsNames) {
-        settings[setting] = document.querySelector("#"+setting).value
+    var element
+    for (let setting of optionNames) {
+        element = document.querySelector("#"+setting)
+        if (element.type == 'checkbox') {
+            settings[setting] = element.checked
+        } else {
+            settings[setting] = element.value
+        }
     }
     browser.storage.local.set(settings)
 
@@ -13,12 +20,34 @@ function saveOptions(e) {
 }
 
 async function restoreOptions() {
-    var settings = await browser.storage.local.get(settingsNames)
+    var settings = await browser.storage.local.get(optionNames)
 
+    var element
     for (let setting in settings) {
-        document.querySelector("#"+setting).value = settings[setting]
+        element = document.querySelector("#"+setting)
+        if (element.type == 'checkbox') {
+            element.checked = settings[setting]
+        } else {
+            element.value = settings[setting]
+        }
     }
 }
 
-document.addEventListener("DOMContentLoaded", restoreOptions)
-document.querySelector("form").addEventListener("submit", saveOptions)
+function updateID(changes) {
+    if (changes.hasOwnProperty('lastEntry')) {
+        var element = document.querySelector('#lastEntry')
+        element.value = changes.lastEntry.newValue
+    }
+}
+
+function resetID(e) {
+    e.preventDefault()
+    browser.storage.local.set({'lastEntry': '0'})
+    var element = document.querySelector('#lastEntry')
+    element.value = '0'
+}
+
+document.addEventListener('DOMContentLoaded', restoreOptions)
+document.querySelector('#save').addEventListener('submit', saveOptions)
+document.querySelector('#reset-id').addEventListener('submit', resetID)
+browser.storage.onChanged.addListener(updateID)
